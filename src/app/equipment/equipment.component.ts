da418df4a0ecbcb5d5d3fbfd088b4f45c8ace65a
deltafiveoh <75449438+deltafiveoh@users.noreply.github.com>
2021-04-01 20:21:03 -0500

    studio 2 complete

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipmentBeingEdited: string = '';
+
+  equipmentList: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(newEquipment: string) {
+    this.equipmentList.push(newEquipment);
+  }
+
+  remove(item: string) {
+    let index = this.equipmentList.indexOf(item);
+    this.equipmentList.splice(index, 1);
+  }
+
+  edit(item: string) {
+    let newItem = item
+    // this.equipmentBeingEdited.replace('', item);
+    this.equipmentBeingEdited = newItem;
+    console.log(this.equipmentBeingEdited)
+ }
+
+ save(replacement: string, item: string) {
+  let index = this.equipmentList.indexOf(item);
+  this.equipmentList.splice(index, 1, replacement);
+  this.equipmentBeingEdited = '';
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experimentBeingEdited: string = '';
+
+  experimentList: string[] = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(newExperiment: string) {
+    console.log(newExperiment)
+    this.experimentList.push(newExperiment);
+  }
+
+  remove(oldExperiment: string) {
+    let index = this.experimentList.indexOf(oldExperiment);
+    this.experimentList.splice(index, 1);
+  }
+
+  edit(oldExperiment: string) {
+    this.experimentBeingEdited = oldExperiment;
+ }
+
+ save(replacement: string, oldExperiment: string) {
+  let index = this.experimentList.indexOf(oldExperiment);
+  this.experimentList.splice(index, 1, replacement);
+  this.experimentBeingEdited = '';
+  }
 }
